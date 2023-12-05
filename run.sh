3693fbc0ceeabc720ff54c5aff27128688a593fd
Karel Malý <105454484+karlosmatos@users.noreply.github.com>
2023-12-05 20:55:37 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Create virtual environment
+python3 -m venv env
+
+# Activate virtual environment
+source env/bin/activate
+
+# Install requirements
+pip install -r requirements.txt
+
+# Run scrapy crawl apx_spider
+scrapy crawl apx_spider
+
+# Run python companies_script.py
+python companies_script.py
+
+# Deactivate virtual environment
+deactivate
