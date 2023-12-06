4842d20a6b3e2edb33dfe03560ad94899b920365
karlosmatos <kmkarelmaly@gmail.com>
2023-12-06 15:39:17 +0100

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,29 +1,17 @@
-# Check Python version
-$pythonVersion = python --version
-$requiredVersion = "Python 3.11"
+# Create virtual environment
+python -m venv env
 
-if ($pythonVersion -like "*$requiredVersion*") {
-    Write-Host "Python 3.11 detected. Proceeding with the script."
+# Activate virtual environment
+. .\env\Scripts\Activate.ps1
 
-    # Create virtual environment
-    python -m venv env
+# Install requirements
+pip install -r requirements.txt
 
-    # Activate virtual environment
-    . .\env\Scripts\Activate.ps1
+# Run scrapy crawl apx_spider
+scrapy crawl apx_spider
 
-    # Install requirements
-    pip install -r requirements.txt
+# Run python companies_script.py
+python .\companies_script.py
 
-    # Run scrapy crawl apx_spider
-    scrapy crawl apx_spider
-
-    # Run python companies_script.py
-    python .\companies_script.py
-
-    # Deactivate virtual environment
-    deactivate
-
-    Write-Host "Script execution completed."
-} else {
-    Write-Host "Python 3.11 is not installed. Please install Python 3.11 to run this script."
-}
+# Deactivate virtual environment
+deactivate
